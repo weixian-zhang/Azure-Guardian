6b4ee611c8faa6f9e4e011d1a06239f438aed1db
weixian-zhang <weixianzhang@outlook.com>
2022-01-26 17:25:15 +0800

    feat(opa-webapi): Added Golang project structure to build OPA wrapper as Rest API

--- src/guardian-core/main.py
+++ src/guardian-core/main.py
@@ -4,8 +4,6 @@ def run():
 
     app = App()
 
-    app.start()
-
 
 if __name__ == '__main__':
     run()
---
+++ src/opa-webapi/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+)
+
+//https://www.openpolicyagent.org/docs/latest/integration/#integrating-with-the-go-api
+
+func main() {
+	 fmt.Println("hello go I am back")
+}
