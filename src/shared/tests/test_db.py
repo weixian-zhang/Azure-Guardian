3b02e338b3afe616d5d85b828a898f9f43458a8a
weixian-zhang <weixianzhang@outlook.com>
2022-02-18 16:30:44 +0800

    unittest(db): Added unit tests for db

--- src/shared/tests/test_db.py
+++ src/shared/tests/test_db.py
@@ -10,9 +10,15 @@ from shared.db import Policy
 class DBTest(unittest.TestCase):
 
     def setUp(self) -> None:
-        pass
+        self.rego = '''
+                        package microsoft.compute.nic.unattached
+
+                        allow = {
+                            allow = true
+                        }
+                    '''
     
-    def test_is_policy_exists_not_exists(self): #, resourceProvider, packageName, desc, username, rego):
+    def test_is_policy_exists_not_exists(self):
 
         with mock.patch("shared.db.PostgreSql.is_policy_exists") as is_policy_exists:
             is_policy_exists.return_value = False, None
@@ -23,7 +29,8 @@ class DBTest(unittest.TestCase):
         self.assertEqual(exists, expected)
         self.assertEqual(policy, None)
 
-    def test_is_policy_exists_exists(self): #, resourceProvider, packageName, desc, username, rego):
+    
+    def test_is_policy_exists_exists(self):
 
         with mock.patch("shared.db.PostgreSql.is_policy_exists") as is_policy_exists:
             is_policy_exists.return_value = True, Policy()
@@ -33,3 +40,49 @@ class DBTest(unittest.TestCase):
 
         self.assertEqual(exists, expected)
         self.assertIsInstance(policy, Policy)
+
+    def test_create_or_update_policy_with_no_existing_policy(self): ##, resourceProvider, packageName, desc, username, rego):
+
+        with mock.patch("shared.db.PostgreSql.is_policy_exists") as is_policy_exists:
+            is_policy_exists.return_value = False, None
+
+            with mock.patch("shared.db.PostgreSql.update_policy") as update_policy:
+                update_policy.return_value = True
+
+                with mock.patch("shared.db.PostgreSql.create_or_update_policy") as create_or_update_policy:
+                    create_or_update_policy.return_value = True
+
+                    resourceProvider = 'microsoft.compute.nic.unattached'
+                    packageName = 'microsoft.compute.nic.unattached'
+                    desc = 'a policy'
+                    username = 'john doe'
+                    rego = self.rego
+                    
+
+                    expected = True
+                    result = create_or_update_policy(resourceProvider, packageName, desc, username, rego)
+
+                    self.assertEqual(result, expected)
+
+    def test_create_or_update_policy_with_existing_policy(self): ##, resourceProvider, packageName, desc, username, rego):
+
+        with mock.patch("shared.db.PostgreSql.is_policy_exists") as is_policy_exists:
+            is_policy_exists.return_value = True, Policy()
+
+            with mock.patch("shared.db.PostgreSql.update_policy") as update_policy:
+                update_policy.return_value = True
+
+                with mock.patch("shared.db.PostgreSql.create_or_update_policy") as create_or_update_policy:
+                    create_or_update_policy.return_value = True
+
+                    resourceProvider = 'microsoft.compute.nic.unattached'
+                    packageName = 'microsoft.compute.nic.unattached'
+                    desc = 'a policy'
+                    username = 'john doe'
+                    rego = self.rego
+
+                    expected = True
+                    result = create_or_update_policy(resourceProvider, packageName, desc, username, rego)
+
+                    self.assertEqual(result, expected)
+
